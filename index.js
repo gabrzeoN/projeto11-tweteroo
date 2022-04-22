8ddff4dbcd3c74b63a7193e50c982fc290bcc701
Gabriel Cari <g.cari@hotmail.com>
2022-04-22 10:47:15 -0300

    Feat: POST/sign-up done.

--- index.js
+++ index.js
@@ -4,8 +4,26 @@ import chalk from "chalk";
 
 const app = express();
 app.use(cors());
-app.listen(5000);
+app.use(express.json());
+app.listen(5000, () => console.log(chalk.bold.green("Server online!")));
 
-app.post("/", (req, res) => {
-    res.send("oi")
+const users = [
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    }
+];
+
+const tweets = [];
+
+
+app.post("/sign-up", (req, res) => {
+    const {username, avatar} = req.body;
+    users.push({username, avatar});
+    res.status(200).json("OK");
+});
+
+app.get("/tweets", (req, res) => {
+    // console.log(req.data);
+    res.status(200).json(tweets)
 });
